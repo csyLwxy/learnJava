217457b4aeb940dcabf292c6beb29ea4acceb18d
wxy <wxy@hust.cn>
2019-09-20 13:47:26 +0800

    update

---
+++ abstractClasses/Student.java
@@ -0,0 +1,17 @@
+package abstractClasses;
+
+public class Student extends Person{
+
+    private String majoy;
+
+    public Student(String name, String majoy) {
+        super(name);
+        this.majoy = majoy;
+    }
+
+    @Override
+    public String getDescription() {
+        return "a student majoring in" + majoy;
+    }
+
+}
