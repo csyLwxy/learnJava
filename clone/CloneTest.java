d09e7b4004ff8b75c3c2444451c3dc57cc9c7d22
wxy <wxy@hust.cn>
2019-09-29 15:50:14 +0800

    update

---
+++ clone/CloneTest.java
@@ -0,0 +1,17 @@
+package clone;
+
+public class CloneTest{
+    public static void main(String[] args) {
+        try{
+            Employee original = new Employee("wxy", 10000);
+            original.setHireDay(2000, 1, 1);
+            Employee copy = original.clone();
+            copy.raiseSalary(10);
+            copy.setHireDay(2002, 2, 2);
+            System.out.println("original=" + original);
+            System.out.println("copy=" + copy);
+        }catch(CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+    }
+}
