7073b4c01849016f39755b1875916d3b310867e5
wxy <wxy209556458@126.com>
2019-11-01 21:00:41 +0800

    update

---
+++ corejava/button/Main.java
@@ -0,0 +1,16 @@
+package button;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main {
+    public static void main(String[] args) {
+            EventQueue.invokeLater(() -> {
+            JFrame frame = new ButtonFrame();
+            frame.setTitle("ButtonFrameTest");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
