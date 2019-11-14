dccac9d5eab2e19bc67fdca5e31c19a6e210ecc1
wxy <wxy209556458@126.com>
2019-11-14 12:06:04 +0800

    update

--- todolist/ToDoList.java
+++ todolist/ToDoList.java
@@ -28,7 +28,7 @@ public class ToDoList extends JFrame {
 		JButton add = new JButton("Add");
 		add.setAlignmentX(Component.CENTER_ALIGNMENT);
 		add.addActionListener(event -> {
-			addTask(this, model);
+			Editor.addTask(this, panel, model);
 		});
 		buttonPanel.add(Box.createVerticalGlue());
 		buttonPanel.add(add);
@@ -39,7 +39,7 @@ public class ToDoList extends JFrame {
 		edit.addActionListener(event -> {
 			int row = table.getSelectedRow();
 			if (row >= 0) {
-				editTask(this, model, row);
+				Editor.editTask(this, panel, model, row);
 			} else {
 				JOptionPane.showMessageDialog(buttonPanel, "You haven't chosen an entry to modify!", "Error",
 						JOptionPane.ERROR_MESSAGE);
@@ -112,95 +112,6 @@ public class ToDoList extends JFrame {
 		add(splitPane);
 		initSet();
 		pack();
-	}
-
-	/**
-	 * 添加task界面
-	 * @param frame
-	 * @param model
-	 */
-	private void addTask(JFrame frame, DefaultTableModel model) {
-
-		JPanel addPanel = new JPanel(new BorderLayout(5, 5));
-		
-		// 输入提示
-		JPanel addLabel = new JPanel(new GridLayout(0, 1, 2, 2));
-		addLabel.add(new JLabel("Title", SwingConstants.RIGHT));
-		addLabel.add(new JLabel("Description", SwingConstants.RIGHT));
-		addLabel.add(new JLabel("Date", SwingConstants.RIGHT));
-		addLabel.add(new JLabel("Priority", SwingConstants.RIGHT));
-		addPanel.add(addLabel, BorderLayout.WEST);
-
-		// 设置输入文本框
-		JPanel addTestFields = new JPanel(new GridLayout(0, 1, 2, 2));
-		JTextField title = new JTextField();
-		addTestFields.add(title);
-		JTextField description = new JTextField();
-		addTestFields.add(description);
-		JTextField date = new JTextField();
-		addTestFields.add(date);
-		JTextField priority = new JTextField();
-		addTestFields.add(priority);
-		addPanel.add(addTestFields, BorderLayout.CENTER);
-
-		int addConfirm = JOptionPane.showConfirmDialog(frame, addPanel, "Add", JOptionPane.OK_CANCEL_OPTION,
-				JOptionPane.PLAIN_MESSAGE);
-		if (addConfirm == JOptionPane.OK_OPTION) {
-			if (!title.getText().isEmpty()) {
-				Object[] row = new Object[4];
-				row[0] = title.getText();
-				row[1] = description.getText();
-				row[2] = date.getText();
-				row[3] = priority.getText();
-				model.addRow(row);
-			} else {
-				JOptionPane.showMessageDialog(panel, "Title is empty!", "Error", JOptionPane.ERROR_MESSAGE);
-			}
-		}
-	}
-
-	/**
-	 * 编辑task的界面
-	 * @param frame
-	 * @param model
-	 * @param n
-	 */
-	private void editTask(JFrame frame, DefaultTableModel model, int n) {
-
-		JPanel editPanel = new JPanel(new BorderLayout(5, 5));
-
-		// 输入提示
-		JPanel editLabel = new JPanel(new GridLayout(0, 1, 2, 2));
-		editLabel.add(new JLabel("Title", SwingConstants.RIGHT));
-		editLabel.add(new JLabel("Description", SwingConstants.RIGHT));
-		editLabel.add(new JLabel("Date", SwingConstants.RIGHT));
-		editLabel.add(new JLabel("Priority", SwingConstants.RIGHT));
-		editPanel.add(editLabel, BorderLayout.WEST);
-
-		// 设置编辑文本框
-		JPanel editTestFields = new JPanel(new GridLayout(0, 1, 2, 2));
-		JTextField title = new JTextField(model.getValueAt(n, 0).toString());
-		editTestFields.add(title);
-		JTextField description = new JTextField(model.getValueAt(n, 1).toString());
-		editTestFields.add(description);
-		JTextField date = new JTextField(model.getValueAt(n, 2).toString());
-		editTestFields.add(date);
-		JTextField priority = new JTextField(model.getValueAt(n, 3).toString());
-		editTestFields.add(priority);
-		editPanel.add(editTestFields, BorderLayout.CENTER);
-
-		int editConfirm = JOptionPane.showConfirmDialog(frame, editPanel, "Edit", JOptionPane.OK_CANCEL_OPTION,
-				JOptionPane.PLAIN_MESSAGE);
-		if (editConfirm == JOptionPane.OK_OPTION) {
-			if (!title.getText().isEmpty()) {
-				model.setValueAt(title.getText(), n, 0);
-				model.setValueAt(description.getText(), n, 1);
-				model.setValueAt(date.getText(), n, 2);
-				model.setValueAt(priority.getText(), n, 3);
-			} else {
-				JOptionPane.showMessageDialog(editPanel, "Title is empty!", "Error", JOptionPane.ERROR_MESSAGE);
-			}
-		}
 	}
 
 	/**
